509f78491cfac8f287aeb70cce2f2bfd1aa8bf2b
mohamdan <engineerm86@gmail.com>
2023-05-27 13:43:28 +0300

    add-image-sound-assets

---
+++ SuperHeros/SuperHeros/SuperHerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHerosApp.swift
+//  SuperHeros
+//
+//  Created by mohamdan on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperHeros/SuperHeros/SuperheroView.swift
@@ -0,0 +1,23 @@
+//
+//  SuperheroView.swift
+//  SuperHeros
+//
+//  Created by mohamdan on 27/05/2023.
+//
+
+import SwiftUI
+
+struct SuperheroView: View {
+    var body: some View {
+        ZStack {
+            Image("hulk")
+            
+        }
+    }
+}
+
+struct SuperheroView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuperheroView()
+    }
+}
