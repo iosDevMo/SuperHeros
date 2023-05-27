d6363e123e3efcc624eb0b87b7fe06553aeedd50
mohamdan <engineerm86@gmail.com>
2023-05-27 14:31:53 +0300

    finsh Hulk view

--- SuperHeros/SuperHeros/SuperheroView.swift
+++ SuperHeros/SuperHeros/SuperheroView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct SuperheroView: View {
+    var gradient : [Color] = [Color("colorHulk01"), Color("colorHulk02")]
     var body: some View {
         ZStack {
             Image("hulk")
+                .resizable()
+                .scaledToFit()
+            VStack{
+                Text("Hulk")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Button {
+                    //add action
+                } label: {
+                    HStack{
+                        Text("Start")
+                        Image(systemName: "arrow.right.circle")
+                    }//:Hstack
+                    .padding()
+                    .font(.title2)
+                    .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .bottomTrailing, endPoint: .topLeading))
+                    .clipShape(Capsule())
+                    .shadow(radius: 10)
+                    
+                }//:label
+
+                
+            }//:vstack
+            .offset(x: 0, y: 150)
             
-        }
+            
+        }//:z stack
+        .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .cornerRadius(16)
+        .shadow(color:.black, radius: 2, x:2, y: 2)
     }
 }
 
